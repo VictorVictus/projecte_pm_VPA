1cd39688f3274504618c2e94bb38abfc6e890707
jorge_larramona_pardina <jorge.larramona@estudiantat.upc.edu>
2025-11-26 11:45:45 +0100

    Estan los modelos en ingles

--- lib/models/playlist.dart
+++ lib/models/playlist.dart
@@ -1,37 +1,43 @@
-class Album {
+class Playlist {
   final String _id;
   String _name;
-  List<String> _artist;
+  String _description;
+  bool _isPublic;
   List<String> _songId;
   final String _creatorId;
   DateTime _createdAt;
 
   //Constructor
-  Album({
+  Playlist({
     required String id,
     required String name,
-    required List<String> artist,
+    String? description,
+    bool? isPublic,
     List<String>? songId,
     required String creatorId,
-    DateTime? createdAt, //Només admin pot escriure aqui
+    DateTime? createdAt, //Nomes admin
   }) : _id = id,
-       _creatorId = creatorId,
        _name = name,
-       _artist = artist,
-       _songId = [],
+       _description = description ?? '',
+       _isPublic = isPublic ?? false,
+       _songId = songId ?? [],
+       _creatorId = creatorId,
        _createdAt = createdAt ?? DateTime.now(); //Guarda data de pujada.
 
   //Llista de getters
   String get id => _id;
-  String get creatorId => _creatorId;
   String get name => _name;
-  List<String> get artist => _artist;
+  String get description => _description;
+  bool get isPublic => _isPublic;
   List<String> get songId => _songId;
+  String get creatorId => _creatorId;
   DateTime get createdAt => _createdAt;
 
   //Llista de Setters
+
   set name(String name) => _name = name;
-  set artist(List<String> artist) => _artist = artist;
+  set description(String description) => _description = description;
+  set isPublic(bool isPublic) => _isPublic = isPublic;
   set songId(List<String> songId) => _songId = songId;
-  set createdAt(DateTime createdAt) => _createdAt = createdAt; //Només admin
+  set createdAt(DateTime createdAt) => _createdAt = createdAt; //Nomes admin
 }
